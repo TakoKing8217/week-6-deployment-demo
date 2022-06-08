e73d18edc57101fe0acd34bbac84d289cda6b486
Nathan Wood <nathan.ronald.wood@gmail.com>
2022-06-08 11:26:56 -0500

    another commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 
-const port = process.env.PORT || 5000;
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+
+const port = process.env.PORT || 5005;
 app.listen(port, () => console.log(`Listening on ${port}`));
